fc91d8d41ffd3a97e10c62816a3ee32ed47bae3c
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2025-02-08 09:54:28 -0500

    typeDefs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-//TODO
+const gql = require("graphql-tag");
+
+const typeDefs = gql'
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+
+}
+';
+
+
+
+module.exports = typeDefs;
